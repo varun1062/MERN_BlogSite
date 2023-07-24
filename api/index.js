39e97aa785a77ca022f380c38cd0e195324aa37d
Varun Ninawe <varunninawe1002@gmail.com>
2023-07-24 23:21:53 +0530

    files upload

---
+++ api/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'shfhxgsfgs6rcsdcdhcjhsgd';
+
+app.use(cors({credentials:true, origin:'http://localhost:3000/'}));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect('mongodb+srv://blog:fL8jUozGV1bE7tVh@cluster0.5ndldw0.mongodb.net/?retryWrites=true&w=majority')
+
+
+
+app.post('/register', async (req,res)=> {
+    
+    const {username,password} = req.body;
+    try {
+        const userDoc = await User.create({
+            username, 
+            password: bcrypt.hashSync(password,salt),
+        });
+    res.json(userDoc);
+    }catch(e) {
+        res.status(400).json(e);
+    }
+    
+
+});
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  });
+
+  app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+      if (err) throw err;
+      res.json(info);
+    });
+  });
+
+// app.get('/test', (req,res)=> {
+//     res.json('test ok');
+// })
+
+app.listen(4000);
+
+// fL8jUozGV1bE7tVh
+
+// mongodb+srv://blog:fL8jUozGV1bE7tVh@cluster0.5ndldw0.mongodb.net/?retryWrites=true&w=majority
